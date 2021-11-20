966f9ac65c4236bcf214ea9368867e74108ebbc2
Kaijonsson <axel.jonsson@hotmail.com>
2021-11-20 14:44:48 +0100

    small visual changes

--- src/mobileAndTablet/components/HeaderMobile.js
+++ src/mobileAndTablet/components/HeaderMobile.js
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../desktop/components/header.css";
-
 import { useHistory } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSortUp, faSortDown } from "@fortawesome/free-solid-svg-icons";
+import "../../index.css";
 
 function HeaderMobile() {
   const history = useHistory();
+  const [showHide, setShowHide] = useState(false);
 
   return (
     <div id="pageTitleContainer">
       <h1 onClick={() => history.push("/")} id="name" className="pageTitle">
         Humlan Lange
       </h1>
-      <h1
-        onClick={() => history.push("/about")}
-        id="about"
-        className="pageTitle"
-      >
-        About
-      </h1>
+      {showHide && (
+        <h1
+          onClick={() => history.push("/about")}
+          id="about"
+          className="pageTitle"
+        >
+          About
+        </h1>
+      )}
+      {showHide ? (
+        <FontAwesomeIcon
+          icon={faSortUp}
+          size="2x"
+          onClick={() => setShowHide(!showHide)}
+          className="mainTitleColor"
+        />
+      ) : (
+        <FontAwesomeIcon
+          icon={faSortDown}
+          size="2x"
+          className="mainFontColor"
+          onClick={() => setShowHide(!showHide)}
+          className="mainTitleColor"
+        />
+      )}
     </div>
   );
 }
